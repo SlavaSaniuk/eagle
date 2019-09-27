8d8c4d8bcc5b3ae100739993d9210d856ca84e8f
SlavaSaniuk <saniuk.vyacheslav.97@gmail.com>
2019-09-27 20:07:38 +0300

    Added login rest controller

---
+++ eagle-authentication/src/main/java/by/bsac/models/User.java
@@ -0,0 +1,14 @@
+package by.bsac.models;
+
+public class User {
+
+    private int user_id;
+
+    public Integer getUserId() {
+        return user_id;
+    }
+
+    public void setUserId(int user_id) {
+        this.user_id = user_id;
+    }
+}
