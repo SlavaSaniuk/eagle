685bc083169b74bf43cb31097481b1222b5782ff
SlavaSaniuk <saniuk.vyacheslav.97@gmail.com>
2019-10-03 15:00:26 +0300

    Added javadoc for property parser service classes

--- eagle-authentication/src/main/java/by/bsac/services/parsers/TypesPropertiesParser.java
+++ eagle-authentication/src/main/java/by/bsac/services/parsers/TypesPropertiesParser.java
@@ -9,6 +9,16 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.InvocationTargetException;
 
+/**
+ * Class extend {@link PropertiesFileParser} class and has a method to cast property value for primitive wrappers types, see example:
+ * <@code>
+ *     //Properties file
+ *     com.example.byte = 69
+ *
+ *     //Class usage
+ *     Byte b = obj.getTypeProperty("com.example.byte", Byte.class);
+ * </@code>
+ */
 public class TypesPropertiesParser extends PropertiesFileParser {
 
     //Logger
@@ -22,8 +32,15 @@ public class TypesPropertiesParser extends PropertiesFileParser {
         super(a_stream);
     }
 
+    /**
+     * Cast property value to primitive wrapper type.
+     * @param property_key - {@link String} key.
+     * @param primitive_wrapper - {@link Class} primitive wrapper.
+     * @return - {@link Object} primitive wrapper with property value.
+     * @throws InvocationTargetException - If installation of wrapper class with property value fails.
+     */
     @SuppressWarnings("unchecked")
-    public Object getTypeProperty(String property_key, Class primitive_wrapper) {
+    public Object getTypeProperty(String property_key, Class primitive_wrapper) throws InvocationTargetException {
 
         if (primitive_wrapper == null) throw new NullPointerException("Primitive wrapper class is null.");
         if (!ClazzUtils.isPrimitiveWrapper(primitive_wrapper)) throw new IllegalArgumentException("Parameter [primitive_wrapper] is not wrapper class of primitive type.");
@@ -34,12 +51,11 @@ public class TypesPropertiesParser extends PropertiesFileParser {
 
         try {
             return primitive_wrapper.getConstructor(String.class).newInstance(property_value);
-        } catch (InstantiationException | InvocationTargetException | NoSuchMethodException | IllegalAccessException e) {
+        } catch (InstantiationException | NoSuchMethodException | IllegalAccessException e) {
             LOGGER.error("This exception should not been thrown.");
             e.printStackTrace();
+            return null;
         }
-
-        return null;
     }
 
 
--- eagle-authentication/src/test/java/service/parser/TypesPropertiesParserTestCase.java
+++ eagle-authentication/src/test/java/service/parser/TypesPropertiesParserTestCase.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.*;
+import java.lang.reflect.InvocationTargetException;
 
 class TypesPropertiesParserTestCase {
 
@@ -23,17 +24,17 @@ class TypesPropertiesParserTestCase {
     }
 
     @Test
-    void getTypeProperty_characterClass_shouldReturnCharacter() throws IOException {
+    void getTypeProperty_characterClass_shouldReturnCharacter() throws IOException, InvocationTargetException {
         Assertions.assertEquals('x', (char) this.parser.getTypeProperty("by.bsac.character", Character.class));
     }
 
     @Test
-    void getTypeProperty_byteClass_shouldReturnByte() throws IOException {
+    void getTypeProperty_byteClass_shouldReturnByte() throws IOException, InvocationTargetException {
         Assertions.assertEquals(69, (byte) this.parser.getTypeProperty("by.bsac.byte", Byte.class));
     }
 
     @Test
-    void getTypeProperty_integerClass_shouldReturnInteger() throws IOException {
+    void getTypeProperty_integerClass_shouldReturnInteger() throws IOException, InvocationTargetException {
         Assertions.assertEquals(1488, (int) this.parser.getTypeProperty("by.bsac.integer", Integer.class));
     }
 
