ceea9d8c4cb763ba0f6da1758acdaf9a23615f99
SlavaSaniuk <saniuk.vyacheslav.97@gmail.com>
2019-10-07 01:02:25 +0300

    Need to test authentication controller

--- eagle-authentication/src/main/java/by/bsac/webmvc/controllers/AuthenticationController.java
+++ eagle-authentication/src/main/java/by/bsac/webmvc/controllers/AuthenticationController.java
@@ -1,13 +1,48 @@
 package by.bsac.webmvc.controllers;
 
+import by.bsac.exceptions.AccountNotRegisteredException;
+import by.bsac.exceptions.EmailAlreadyRegisteredException;
+import by.bsac.models.Account;
+import by.bsac.models.User;
+import by.bsac.services.accounts.AccountManagementService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
-@RequestMapping("/")
+@RestController("AuthenticationController")
 public class AuthenticationController {
 
+    //Logger
+    private static final Logger LOGGER = LoggerFactory.getLogger(AuthenticationController.class);
+    //Spring beans
+    private AccountManagementService ams; //Autowired via setter
 
+    @PostMapping("/register")
+    public User register(@RequestBody Account account) {
 
+        try{
+            return this.ams.register(account);
+        }catch (EmailAlreadyRegisteredException exc) {
+            LOGGER.debug(exc.getMessage());
+            return null;
+        }
+    }
 
+    @PostMapping("/login")
+    public User login(@RequestBody Account account) {
+        try{
+            return this.ams.login(account);
+        }catch (AccountNotRegisteredException exc) {
+            LOGGER.debug(exc.getMessage());
+            return null;
+        }
+    }
+
+    @Autowired
+    public void setAccountManagementService(AccountManagementService a_ams) {
+        LOGGER.debug("[AUTOWIRE] " +a_ams.getClass().getSimpleName() +" to " +getClass().getSimpleName() +" controller bean.");
+        this.ams = a_ams;
+    }
 }
