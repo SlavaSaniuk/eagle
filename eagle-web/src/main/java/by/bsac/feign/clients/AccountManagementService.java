b8a43e778b927e8c91f1c53bd027d6dcaabcc99b
SlavaSaniuk <saniuk.vyacheslav.97@gmail.com>
2019-10-08 13:36:44 +0300

    Deployed eagle-auth in docker

--- eagle-web/src/main/java/by/bsac/feign/clients/AccountManagementService.java
+++ eagle-web/src/main/java/by/bsac/feign/clients/AccountManagementService.java
@@ -2,6 +2,6 @@ package by.bsac.feign.clients;
 
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient(name = "AccountManagement", url = "http://10.8.8.2:80/eagle-authentication") //Disabled, because feign clients scanning is not configured
+@FeignClient(name = "AccountManagement", url = "http://10.8.8.20:36547/eagle-auth") //Disabled, because feign clients scanning is not configured
 public interface AccountManagementService {
 }
