199431a0bf8cb6c70c41d05239e1d813fae85b0b
SlavaSaniuk <saniuk.vyacheslav.97@gmail.com>
2019-10-09 14:53:05 +0300

    Added jackson encoder/decoder, object mapper beans

--- eagle-web/src/main/java/by/bsac/feign/FeignConfiguration.java
+++ eagle-web/src/main/java/by/bsac/feign/FeignConfiguration.java
@@ -2,7 +2,11 @@ package by.bsac.feign;
 
 import by.bsac.conf.LoggerDefaultLogs;
 import by.bsac.feign.clients.AccountManagementService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import feign.Feign;
+import feign.codec.Decoder;
+import feign.codec.Encoder;
 import feign.jackson.JacksonDecoder;
 import feign.jackson.JacksonEncoder;
 import org.slf4j.Logger;
@@ -25,10 +29,44 @@ public class FeignConfiguration {
     @Bean(name = "AccountManagementService")
     public AccountManagementService accountManagementService() {
         LOGGER.debug(String.format(LoggerDefaultLogs.CREATE_BEAN_START, AccountManagementService.class.getSimpleName()));
-        AccountManagementService ams = Feign.builder().encoder(new JacksonEncoder()).decoder(new JacksonDecoder()).target(AccountManagementService.class, "http://10.8.8.20:36547/eagle-auth/");
+
+        AccountManagementService ams = Feign.builder()
+                .encoder(this.jacksonEncoder())
+                .decoder(this.jacksonDecoder())
+                .target(AccountManagementService.class, "http://10.8.8.20:36547/eagle-auth/");
 
         LOGGER.debug(String.format(LoggerDefaultLogs.CREATE_BEAN_FINISH, ams.getClass().getSimpleName()));
         return ams;
+    }
+
+    @Bean("JacksonEncoder")
+    public Encoder jacksonEncoder() {
+        LOGGER.debug(String.format(LoggerDefaultLogs.CREATE_BEAN_START, JacksonEncoder.class.getSimpleName()));
+        final JacksonEncoder encoder = new JacksonEncoder(this.jacksonObjectMapper());
+
+        LOGGER.debug(String.format(LoggerDefaultLogs.CREATE_BEAN_FINISH, encoder.getClass().getSimpleName()));
+        return encoder;
+    }
+
+    @Bean("jacksonDecoder")
+    public Decoder jacksonDecoder() {
+        LOGGER.debug(String.format(LoggerDefaultLogs.CREATE_BEAN_START, JacksonDecoder.class.getSimpleName()));
+        JacksonDecoder decoder = new JacksonDecoder(this.jacksonObjectMapper());
+
+        LOGGER.debug(String.format(LoggerDefaultLogs.CREATE_BEAN_FINISH, decoder.getClass().getSimpleName()));
+        return decoder;
+    }
+
+    @Bean("ObjectMapper")
+    public ObjectMapper jacksonObjectMapper() {
+
+        LOGGER.debug(String.format(LoggerDefaultLogs.CREATE_BEAN_START, ObjectMapper.class.getSimpleName()));
+        ObjectMapper mapper = new ObjectMapper();
+
+        mapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
+
+        LOGGER.debug(String.format(LoggerDefaultLogs.CREATE_BEAN_FINISH, mapper.getClass().getSimpleName()));
+        return mapper;
     }
 
 
