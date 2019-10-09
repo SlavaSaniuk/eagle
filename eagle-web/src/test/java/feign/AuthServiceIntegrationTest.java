226cb4b917687ea6a9f5369d9112bb54646e58d9
SlavaSaniuk <saniuk.vyacheslav.97@gmail.com>
2019-10-09 23:25:38 +0300

    Worked test for auth service

--- eagle-web/src/test/java/feign/AuthServiceIntegrationTest.java
+++ eagle-web/src/test/java/feign/AuthServiceIntegrationTest.java
@@ -5,15 +5,15 @@ import by.bsac.feign.FeignConfiguration;
 import by.bsac.feign.clients.AccountManagementService;
 import by.bsac.models.Account;
 import by.bsac.models.User;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 @SpringJUnitConfig({RootContextConfiguration.class, FeignConfiguration.class})
-public class AuthServiceIntegrationTest {
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class AuthServiceIntegrationTest {
 
     //Logger
     private static final Logger LOGGER = LoggerFactory.getLogger(AuthServiceIntegrationTest.class);
@@ -22,9 +22,10 @@ public class AuthServiceIntegrationTest {
     private AccountManagementService ams;
 
     @Test
+    @Order(1)
     void register_newAccount_shouldReturnNewAccountUser() {
         Account account = new Account();
-        account.setAccountEmail("test1488@eagle-web.com");
+        account.setAccountEmail("test1@eagle-web.com");
         account.setAccountPassword("12345678");
 
         User user = this.ams.registerAccount(account);
@@ -36,9 +37,10 @@ public class AuthServiceIntegrationTest {
     }
 
     @Test
+    @Order(2)
     void login_registeredAccount_shouldReturnUserAccount() {
         Account account = new Account();
-        account.setAccountEmail("test1488@eagle-web.com");
+        account.setAccountEmail("test1@eagle-web.com");
         account.setAccountPassword("12345678");
 
         User user = this.ams.loginAccount(account);
@@ -52,9 +54,10 @@ public class AuthServiceIntegrationTest {
     }
 
     @Test
+    @Order(3)
     void login_incorrectPassword_shouldReturnInvalidUserEntity() {
         Account account = new Account();
-        account.setAccountEmail("test1488@eagle-web.com");
+        account.setAccountEmail("test1@eagle-web.com");
         account.setAccountPassword("invalid-password");
 
         User user = this.ams.loginAccount(account);
