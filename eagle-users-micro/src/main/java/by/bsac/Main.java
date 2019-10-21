ed7a89ad21b21b33916f925d05df204cc778d1e3
SlavaSaniuk <saniuk.vyacheslav.97@gmail.com>
2019-10-21 17:06:09 +0300

    Added eagle-users-micro microservice

--- eagle-users-micro/src/main/java/by/bsac/Main.java
+++ eagle-users-micro/src/main/java/by/bsac/Main.java
@@ -1,11 +1,24 @@
 package by.bsac;
 
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
-public class Main {
+@SpringBootApplication(scanBasePackages = "by.bsac.webmvc.controllers")
+public class Main extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+
+        //Disable banner
+        builder.bannerMode(Banner.Mode.OFF);
+
+        return builder;
     }
 }
---
+++ eagle-users-micro/src/main/java/by/bsac/models/User.java
@@ -0,0 +1,17 @@
+package by.bsac.models;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class User {
+
+    @Setter
+    private Integer user_id;
+
+    @Setter
+    private String user_id_alias;
+
+}
