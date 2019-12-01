1a1e089327627573ce88eb9a9a4958f626b63b39
SlavaSaniuk <saniuk.vyacheslav.97@gmail.com>
2019-12-01 11:09:06 +0300

    Worked UserWithDetails converter tests

--- eagle-web/src/test/java/converters/UserWithDetailsDtoConverterTestCase.java
+++ eagle-web/src/test/java/converters/UserWithDetailsDtoConverterTestCase.java
@@ -1,6 +1,6 @@
 package converters;
 
-import by.bsac.core.beans.DtoEntityConverter;
+import by.bsac.core.beans.EmbeddedDeConverter;
 import by.bsac.core.beans.EmbeddedDtoEntityConverter;
 import by.bsac.core.exceptions.NoSupportedEntitiesException;
 import by.bsac.models.User;
@@ -11,10 +11,13 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class UserWithDetailsDtoConverterTestCase {
 
-    private DtoEntityConverter<UserWithDetailsDto> CONVERTER;
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserWithDetailsDtoConverterTestCase.class);
+    private EmbeddedDeConverter<UserWithDetailsDto> CONVERTER;
 
     @BeforeEach
     public void setUpBeforeEach() throws NoSupportedEntitiesException {
@@ -42,5 +45,28 @@ public class UserWithDetailsDtoConverterTestCase {
         Assertions.assertEquals(details.getUserName().getFirstName(), dto.getUserFname());
         Assertions.assertEquals(details.getUserName().getLastName(), dto.getUserLname());
 
+        LOGGER.debug(dto.toString());
     }
+
+    @Test
+    public void toEntity_newUserWithDetailsDto_shouldReturnUserAndDetailsEntities() {
+
+        final UserWithDetailsDto dto = new UserWithDetailsDto();
+        dto.setUserFname("FIRST NAME");
+        dto.setUserLname("LAST NAME");
+        dto.setUserId(24);
+
+        User user = this.CONVERTER.toEntity(dto, new User());
+        UserDetails details = this.CONVERTER.toEntity(dto, new UserDetails(), new UserName());
+
+        Assertions.assertEquals(dto.getUserId(), user.getUserId());
+        Assertions.assertEquals(dto.getUserFname(), details.getUserName().getFirstName());
+        Assertions.assertEquals(dto.getUserLname(), details.getUserName().getLastName());
+
+        LOGGER.debug(user.toString());
+        LOGGER.debug(details.toString());
+
+    }
+
+
 }
