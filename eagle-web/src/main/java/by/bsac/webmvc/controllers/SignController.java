053c19a6877b234bf67ac73f2de88247a90e61cd
SlavaSaniuk <saniuk.vyacheslav.97@gmail.com>
2019-12-04 22:52:36 +0300

    Worked SignController and UserController login method

--- eagle-web/src/main/java/by/bsac/webmvc/controllers/SignController.java
+++ eagle-web/src/main/java/by/bsac/webmvc/controllers/SignController.java
@@ -1,11 +1,16 @@
 package by.bsac.webmvc.controllers;
 
-import by.bsac.conf.LoggerDefaultLogs;
+import by.bsac.core.beans.EmbeddedDeConverter;
 import by.bsac.exceptions.AccountAlreadyRegisteredException;
 import by.bsac.exceptions.AccountNotRegisteredException;
+import by.bsac.exceptions.NoCreatedDetailsException;
 import by.bsac.feign.clients.AccountManagementService;
+import by.bsac.feign.clients.UserDetailsService;
 import by.bsac.models.Account;
 import by.bsac.models.User;
+import by.bsac.models.UserDetails;
+import by.bsac.models.UserName;
+import by.bsac.webmvc.dto.UserWithDetailsDto;
 import by.bsac.webmvc.forms.AccountForm;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,6 +22,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
+import static by.bsac.conf.LoggerDefaultLogs.*;
+
 @Controller
 @SessionAttributes({"common_user"})
 public class SignController {
@@ -25,6 +32,8 @@ public class SignController {
     private static final Logger LOGGER = LoggerFactory.getLogger(SignController.class);
     //Spring beans
     private AccountManagementService ams; //Autowired via setter
+    private UserDetailsService uds; //Autowired via setter
+    private EmbeddedDeConverter<UserWithDetailsDto> details_converter; //Autowired via setter
 
     //Model attributes
     @ModelAttribute("AccountForm")
@@ -41,7 +50,7 @@ public class SignController {
     }
 
     @PostMapping("/signin")
-    public ModelAndView loginAccount(@ModelAttribute("AccountForm") AccountForm account_form) {
+    public ModelAndView loginAccount(@ModelAttribute("AccountForm") AccountForm account_form) throws NoCreatedDetailsException {
         //Create mav
         ModelAndView mav = new ModelAndView();
 
@@ -55,6 +64,14 @@ public class SignController {
             mav.setViewName("redirect://sign?form=signup");
             return mav;
         }
+
+        //Get user details
+        UserWithDetailsDto dto = new UserWithDetailsDto();
+        dto.setUserId(account_user.getUserId());
+        dto = this.uds.getDetails(dto);
+        UserDetails details = this.details_converter.toEntity(dto, new UserDetails(), new UserName());
+        account_user.setUserDetails(details);
+        details.setDetailsUser(account_user);
 
         //Put user attribute to session
         mav.getModel().put("common_user", account_user);
@@ -86,10 +103,23 @@ public class SignController {
         return mav;
     }
 
+    //Autowiring
     @Autowired
     public void setAccountManagementService(AccountManagementService a_ams) {
-        LOGGER.debug(LoggerDefaultLogs.AUTOWIRING.viaSetter(a_ams.getClass(), this.getClass()));
+        LOGGER.debug(AUTOWIRING.viaSetter(a_ams.getClass(), this.getClass()));
         this.ams = a_ams;
+    }
+
+    @Autowired
+    public void setUserDetailsService(UserDetailsService a_uds) {
+        LOGGER.debug(AUTOWIRING.viaSetter(a_uds.getClass(), this.getClass()));
+        this.uds = a_uds;
+    }
+
+    @Autowired
+    public void setDetailsConverter(EmbeddedDeConverter<UserWithDetailsDto> a_converter) {
+        LOGGER.debug(AUTOWIRING.viaSetter(a_converter.getClass(), this.getClass()));
+        this.details_converter = a_converter;
     }
 
 }
--- eagle-web/src/main/java/by/bsac/webmvc/controllers/UserController.java
+++ eagle-web/src/main/java/by/bsac/webmvc/controllers/UserController.java
@@ -1,8 +1,15 @@
 package by.bsac.webmvc.controllers;
 
+import by.bsac.core.beans.EmbeddedDeConverter;
+import by.bsac.exceptions.NoCreatedDetailsException;
+import by.bsac.feign.clients.UserDetailsService;
 import by.bsac.models.User;
+import by.bsac.models.UserDetails;
+import by.bsac.models.UserName;
+import by.bsac.webmvc.dto.UserWithDetailsDto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +22,9 @@ import static by.bsac.conf.LoggerDefaultLogs.*;
 public class UserController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
+    //Spring beans
+    private UserDetailsService uds; //Autowired via setter
+    private EmbeddedDeConverter<UserWithDetailsDto> details_converter; //Autowired via setter
 
     public UserController() {
         LOGGER.debug(CREATION.beanCreationStart(this.getClass()));
@@ -23,7 +33,7 @@ public class UserController {
 
 
     @GetMapping(path = "/user_{given_id}")
-    public ModelAndView getGivenUserPage(@SessionAttribute("common_user") User common_user, @PathVariable Integer given_id) {
+    public ModelAndView getGivenUserPage(@SessionAttribute("common_user") User common_user, @PathVariable Integer given_id) throws NoCreatedDetailsException {
         final ModelAndView mav = new ModelAndView();
 
         //Check whether user access your own page
@@ -32,8 +42,18 @@ public class UserController {
             return mav;
         }
 
+        //Get given user details
+        UserWithDetailsDto dto = new UserWithDetailsDto();
+        dto.setUserId(given_id);
 
+        dto = this.uds.getDetails(dto);
+        User given_user = this.details_converter.toEntity(dto, new User());
+        UserDetails given_details = this.details_converter.toEntity(dto, new UserDetails(), new UserName());
 
+        given_user.setUserDetails(given_details);
+        given_details.setDetailsUser(given_user);
+
+        mav.getModel().put("given_user", given_user);
 
         mav.setViewName("given_user_page");
         return mav;
@@ -47,7 +67,18 @@ public class UserController {
         return mav;
     }
 
+    //Autowiring
+    @Autowired
+    public void setUserDetailsService(UserDetailsService a_uds) {
+        LOGGER.debug(AUTOWIRING.viaSetter(a_uds.getClass(), this.getClass()));
+        this.uds = a_uds;
+    }
 
+    @Autowired
+    public void setUserDetailsConverter(EmbeddedDeConverter<UserWithDetailsDto> converter) {
+        LOGGER.debug(AUTOWIRING.viaSetter(converter.getClass(), this.getClass()));
+        this.details_converter = converter;
+    }
 
 
 
