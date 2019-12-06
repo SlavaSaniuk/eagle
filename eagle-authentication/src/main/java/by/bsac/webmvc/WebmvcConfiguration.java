427b4d97871c842ff2f15994d5257f2617e81c6c
SlavaSaniuk <saniuk.vyacheslav.97@gmail.com>
2019-12-06 07:32:50 +0300

    Integrated DTO converters

--- eagle-authentication/src/main/java/by/bsac/webmvc/WebmvcConfiguration.java
+++ eagle-authentication/src/main/java/by/bsac/webmvc/WebmvcConfiguration.java
@@ -4,12 +4,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @ComponentScan("by.bsac.webmvc.controllers")
 @EnableWebMvc
+@Import(DtoConvertersConfiguration.class)
 public class WebmvcConfiguration implements WebMvcConfigurer {
 
     //Logger
