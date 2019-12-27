963a7ae51a39d73e06be31bcc76c102cffac8387
SlavaSaniuk <saniuk.vyacheslav.97@gmail.com>
2019-12-28 00:38:03 +0300

    Added, configured and tested ObjectMapper bean. Worked AccountsStatusesManagementController controller.

--- eagle-authentication/src/test/java/by/bsac/controllers/AccountsStatusesManagementControllerIntegrationTest.java
+++ eagle-authentication/src/test/java/by/bsac/controllers/AccountsStatusesManagementControllerIntegrationTest.java
@@ -8,13 +8,13 @@ import by.bsac.services.ServicesConfiguration;
 import by.bsac.webmvc.DtoConvertersConfiguration;
 import by.bsac.webmvc.WebmvcConfiguration;
 import by.bsac.webmvc.dto.AccountWithStatusDto;
-import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
@@ -26,6 +26,7 @@ import org.springframework.web.context.WebApplicationContext;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
 @ActiveProfiles("TEST")
 @Sql("classpath:/repositories/account-imports.sql" )
 @SpringJUnitWebConfig(classes = {WebmvcConfiguration.class, DtoConvertersConfiguration.class, ServicesConfiguration.class,
@@ -34,13 +35,12 @@ public class AccountsStatusesManagementControllerIntegrationTest {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(AccountsStatusesManagementControllerIntegrationTest.class);
     private MockMvc mock_mvc;
-    private final ObjectMapper mapper = new ObjectMapper();
+    @Autowired
+    private ObjectMapper mapper;
 
     @BeforeEach
     void setUpBeforeEach(WebApplicationContext wac) {
         this.mock_mvc = MockMvcBuilders.webAppContextSetup(wac).build();
-        this.mapper.enable(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT);
-        this.mapper.enable(DeserializationFeature.ACCEPT_EMPTY_ARRAY_AS_NULL_OBJECT);
     }
 
     @Test
@@ -66,7 +66,6 @@ public class AccountsStatusesManagementControllerIntegrationTest {
         LOGGER.debug("Result DTO: " +result_dto.toString());
 
         Assertions.assertEquals(Status.CONFIRMED, result_dto.getAccountStatus());
-
 
     }
 }
