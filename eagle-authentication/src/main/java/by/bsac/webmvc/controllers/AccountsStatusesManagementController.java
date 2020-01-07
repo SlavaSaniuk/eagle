f2873285b116df47c8627faf48c7659581943410
SlavaSaniuk <saniuk.vyacheslav.97@gmail.com>
2020-01-07 19:15:46 +0300

    Added maven test and production profiles for eagle-authentication ms.

--- eagle-authentication/src/main/java/by/bsac/webmvc/controllers/AccountsStatusesManagementController.java
+++ eagle-authentication/src/main/java/by/bsac/webmvc/controllers/AccountsStatusesManagementController.java
@@ -11,10 +11,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import static by.bsac.conf.LoggerDefaultLogs.*;
 
@@ -34,7 +31,7 @@ public class AccountsStatusesManagementController {
     }
 
     @MethodCall(withArgs = true, withStartTime = true) @MethodExecutionTime(inMicros = true, inNanos = false, inMillis = true)
-    @GetMapping(value = "/confirm_account", headers = "content-type=application/json", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "/confirm_account", headers = "content-type=application/json", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody public AccountWithStatusDto confirmAccount(@RequestBody AccountWithStatusDto dto) {
 
         Account account_to_confirm = this.dto_converter.toEntity(dto, new Account());
