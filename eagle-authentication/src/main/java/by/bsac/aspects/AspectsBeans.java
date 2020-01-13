509f015be14a971154d51bf0290a9cb332c25387
SlavaSaniuk <saniuk.vyacheslav.97@gmail.com>
2020-01-14 01:14:59 +0300

    Integrated AspectJ load-time weaving

---
+++ eagle-authentication/src/main/java/by/bsac/aspects/AspectsBeans.java
@@ -0,0 +1,49 @@
+package by.bsac.aspects;
+
+import by.bsac.aspects.debug.MethodCallAspect;
+import by.bsac.aspects.debug.MethodExecutionTimeAspect;
+import by.bsac.core.debugging.LoggerLevel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import static by.bsac.core.logging.SpringCommonLogging.*;
+
+@SuppressWarnings("AccessStaticViaInstance")
+@Configuration("AspectsBeans")
+public class AspectsBeans {
+
+    //Logger
+    private static final Logger LOGGER = LoggerFactory.getLogger(AspectsBeans.class);
+
+    public AspectsBeans() {
+        LOGGER.info(INITIALIZATION.startInitializeConfiguration(AspectsBeans.class));
+    }
+
+    @Bean(name = "MethodCallAspect")
+    @Profile("ASPECTS_DEBUG")
+    public MethodCallAspect getMethodCallAspect() {
+
+        LOGGER.info(CREATION.startCreateBean(BeanDefinition.of("MethodCallAspect").ofClass(MethodCallAspect.class).forProfile("ASPECTS_DEBUG")));
+        MethodCallAspect aspect = MethodCallAspect.aspectOf();
+
+        aspect.setLoggerLevel(LoggerLevel.INFO);
+
+        LOGGER.info(CREATION.endCreateBean(BeanDefinition.of("MethodCallAspect").ofClass(MethodCallAspect.class).forProfile("ASPECTS_DEBUG")));
+        return aspect;
+    }
+
+    @Bean(name = "MethodExecutionTimeAspect")
+    @Profile("ASPECTS_DEBUG")
+    public MethodExecutionTimeAspect getMethodExecutionTimeAspect() {
+        LOGGER.info(CREATION.startCreateBean(BeanDefinition.of("MethodExecutionTimeAspect").ofClass(MethodExecutionTimeAspect.class).forProfile("ASPECTS_DEBUG")));
+        MethodExecutionTimeAspect aspect = MethodExecutionTimeAspect.aspectOf();
+
+        aspect.setLoggerLevel(LoggerLevel.INFO);
+
+        LOGGER.info(CREATION.endCreateBean(BeanDefinition.of("MethodExecutionTimeAspect").ofClass(MethodExecutionTimeAspect.class).forProfile("ASPECTS_DEBUG")));
+        return aspect;
+    }
+}
